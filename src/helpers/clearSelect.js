bee433f23dd608796456afef0659ed10bdecebfd
Tatiana Kashcha <tkashcha@gmail.com>
2023-11-08 10:15:21 +0200

    Update clearSelect.js

--- src/helpers/clearSelect.js
+++ src/helpers/clearSelect.js
@@ -1,11 +1,3 @@
-// Доступ до select напряму в DOM (не рекомендується)
-// export function clearSelect() {
-//   let selectInputBrand = document.getElementById('inputBrandText');
-//   let selectInputPrice = document.getElementById('inputPriceTo');
-//   selectInputBrand.innerHTML += '';
-//   selectInputPrice.innerHTML += '';
-// }
-
 // Доступ до select за допомогою useRef
 export function clearSelect(selectInputBrandRef, selectInputPriceRef) {
   selectInputBrandRef.current.innerHTML += '';
