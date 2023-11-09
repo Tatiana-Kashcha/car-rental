8256ae64a3f8bfd06bef6576f23916bc5de4b3f1
Tatiana Kashcha <tkashcha@gmail.com>
2023-11-09 18:30:59 +0200

    Update CatalogList.styled.js

--- src/components/CatalogList/CatalogList.styled.js
+++ src/components/CatalogList/CatalogList.styled.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import { globalStyled } from 'stylesheet/styled';
 
 export const List = styled.ul`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
   flex-wrap: wrap;
   padding: 0px;
@@ -21,18 +22,17 @@ export const LoadMore = styled.a`
   display: flex;
   justify-content: center;
   border: none;
-  color: rgba(52, 112, 255, 1);
+  color: ${globalStyled.colors.accent};
   font-weight: 500;
   font-size: 16px;
   line-height: 1.5;
-  color: rgba(52, 112, 255, 1);
   text-decoration: underline;
   margin-bottom: 30px;
   margin-top: 50px;
   transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &:hover,
   &:focus {
-    color: rgba(11, 68, 205, 1);
+    color: ${globalStyled.colors.accentHover};
     cursor: pointer;
   }
 `;
